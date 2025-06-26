129b207fbe1bedfefc41eecf1f87fe5abe145a1a
Avazbek Qalandarov <qalandarovavazbek43@gmail.com>
2025-06-26 12:30:34 +0500

    test

--- validator/imageValidator/imageValidate.js
+++ validator/imageValidator/imageValidate.js
@@ -1,8 +1,17 @@
 import Joi from "joi";
 
 export const imageSchema = Joi.object({
-    originalname: Joi.string().regex(/\.(jpg|jpeg|png|webp)$/i).required()/* .message('Rasm turi notoʻgʻri') */,
-    mimetype: Joi.string().valid("image/jpeg", "image/png", "image/gif", "image/webp").required()/* .message('Rasm formati notoʻgʻri') */,
-    size: Joi.number().max(5 * 1024 * 1024).required()/* .message('Rasm hajmi 5 mb dan oshmasligi kerak!') */,
-    fieldname: Joi.string().valid('image').required()/* .message("Rasm yuklanmadi!") */,
+    originalname: Joi.string().regex(/\.(jpg|jpeg|png|webp)$/i).required().messages({
+        "string.pattern.base": "Rasm nomi noto'g'ri formatda!",
+        "any.required": "Rasm nomi talab qilinadi!"
+    }),
+    mimetype: Joi.string().valid("image/jpeg", "image/png", "image/gif", "image/webp").required().messages({
+        "any.required": "Rasm formati talab qilinadi!"
+    }),
+    size: Joi.number().max(5 * 1024 * 1024).required().messages({
+        "number.max": "Rasm hajmi 5 mb dan oshmasligi kerak!"
+    }),
+    fieldname: Joi.string().valid('image').required().messages({
+        "any.required": "Rasm yuklanmadi!"
+    }),
 })
