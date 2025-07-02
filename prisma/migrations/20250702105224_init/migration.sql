6a854566edd93e80a10249a113a6feb526e24d40
Avazbek Qalandarov <qalandarovavazbek43@gmail.com>
2025-07-02 15:57:37 +0500

    test

---
+++ prisma/migrations/20250702105224_init/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Admins" ADD COLUMN     "image_path" TEXT;
+
+-- AlterTable
+ALTER TABLE "Workers" ADD COLUMN     "image_path" TEXT;
--- router/admin/adminRouter.js
+++ router/admin/adminRouter.js
@@ -2,12 +2,11 @@ import express from "express"
 import { adminCreate, deleteAdmin, getAllAdmins, getOneAdmin, updateAdmin, updatePassword } from "../../controller/admin.controller.js"
 import { authorization } from "../../controller/auth.controller.js"
 import upload from "../../helper/multer.js"
-// import { upload, uploadImage } from "../../helper/supabase.js"
 
 const router = express.Router()
 
 router
-    .post('/admin/create', authorization('SUPERADMIN', 'ADMIN'), /* upload.single('image'), uploadImage, */ upload.single('image'), adminCreate)
+    .post('/admin/create', authorization('SUPERADMIN', 'ADMIN'), upload.single('image'), adminCreate)
     .get('/admins', authorization('ADMIN', 'SUPERADMIN'), getAllAdmins)
     .get('/admin/:id', authorization('SUPERADMIN'), getOneAdmin)
     .put('/admin/:id/update', authorization('SUPERADMIN'), updateAdmin)
