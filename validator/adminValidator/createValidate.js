a99f0395262777312f5fd1fb0521df41469c5898
Avazbek Qalandarov <qalandarovavazbek43@gmail.com>
2025-07-07 15:07:04 +0500

    valildatsiya o'zgartirildi

--- validator/adminValidator/createValidate.js
+++ validator/adminValidator/createValidate.js
@@ -1,31 +1,36 @@
 import Joi from "joi";
 
 export const AdminCreateSchema = Joi.object({
-    name: Joi.string().min(3).max(255).required().messages({
-        'string.base': 'Ism faqat matn boʻlishi kerak',
+    name: Joi.string().min(3).max(255).empty('').required().messages({
+        'string.base': 'Ism faqat matn boʻlishi kerak!',
+        'string.empty': 'Ism kiritilmagan!',
         'string.min': 'Ism kamida {#limit} ta belgidan iborat boʻlishi kerak!',
         'string.max': 'Ism eng koʻpi bilan {#limit} ta belgidan iborat boʻlishi kerak!',
         'any.required': 'Ism majburiy maydon!'
     }),
-    username: Joi.string().min(3).max(255).trim().required().messages({
-        'string.base': 'Username faqat matn boʻlishi kerak',
+    username: Joi.string().min(3).max(255).empty('').trim().required().messages({
+        'string.base': 'Username faqat matn boʻlishi kerak!',
+        'string.empty': 'Username kiritilmagan!',
         'string.min': 'Username kamida {#limit} ta belgidan iborat boʻlishi kerak!',
         'string.max': 'Username eng koʻpi bilan {#limit} ta belgidan iborat boʻlishi kerak!',
         'any.required': 'Username majburiy maydon!'
     }),
-    password: Joi.string().min(8).max(15).trim().required().messages({
-        'string.base': 'Parol faqat matn boʻlishi kerak',
+    password: Joi.string().min(8).max(15).empty('').trim().required().messages({
+        'string.base': 'Parol faqat matn boʻlishi kerak!',
+        'string.empty': 'Parol kiritilmagan!',
         'string.min': 'Parol kamida {#limit} ta belgidan iborat boʻlishi kerak!',
         'string.max': 'Parol eng koʻpi bilan {#limit} ta belgidan iborat boʻlishi kerak!',
         'any.required': 'Parol majburiy maydon!'
     }),
-    phone: Joi.string().trim().required().messages({
-        'string.base': 'Telefon raqam faqat matn boʻlishi kerak',
+    phone: Joi.string().trim().empty('').required().messages({
+        'string.base': 'Telefon raqam faqat matn boʻlishi kerak!',
+        'string.empty': 'Telefon raqam kiritilmagan!',
         'any.required': 'Telefon raqam majburiy maydon!'
     }),
-    role: Joi.string().valid("ADMIN", "SUPERADMIN").required().messages({
-        'string.base': 'Role faqat matn boʻlishi kerak',
-        'any.only': 'Role faqat ADMIN yoki SUPERADMIN bo‘lishi mumkin',
+    role: Joi.string().empty('').valid("ADMIN", "SUPERADMIN").required().messages({
+        'string.base': 'Role faqat matn boʻlishi kerak!',
+        'string.base': 'Role kiritilmagan!',
+        'any.only': 'Role faqat ADMIN yoki SUPERADMIN bo‘lishi mumkin!',
         'any.required': 'Role majburiy maydon!'
     })
 })
--- validator/adminValidator/updateValidate.js
+++ validator/adminValidator/updateValidate.js
@@ -1,25 +1,29 @@
 import Joi from "joi";
 
 export const updateAdminSchema = Joi.object({
-    name: Joi.string().min(3).max(255).messages({
-        'string.base': 'Ism faqat matn boʻlishi kerak',
+    name: Joi.string().min(3).max(255).empty("").messages({
+        'string.base': 'Ism faqat matn boʻlishi kerak!',
+        'string.empty': 'Ism kiritilmagan!',
         'string.min': 'Ism kamida {#limit} ta belgidan iborat boʻlishi kerak!',
         'string.max': 'Ism eng koʻpi bilan {#limit} ta belgidan iborat boʻlishi kerak!',
         'any.required': 'Ism majburiy maydon!'
     }),
-    username: Joi.string().min(3).max(255).trim().messages({
-        'string.base': 'Username faqat matn boʻlishi kerak',
+    username: Joi.string().min(3).max(255).empty('').trim().messages({
+        'string.base': 'Username faqat matn boʻlishi kerak!',
+        'string.empty': 'Username kiritilmagan!',
         'string.min': 'Username kamida {#limit} ta belgidan iborat boʻlishi kerak!',
         'string.max': 'Username eng koʻpi bilan {#limit} ta belgidan iborat boʻlishi kerak!',
         'any.required': 'Username majburiy maydon!'
     }),
-    phone: Joi.string().trim().messages({
-        'string.base': 'Telefon raqam faqat matn boʻlishi kerak',
+    phone: Joi.string().empty('').trim().messages({
+        'string.base': 'Telefon raqam faqat matn boʻlishi kerak!',
+        'string.empty': 'Telefon raqam kiritilmagan!',
         'any.required': 'Telefon raqam majburiy maydon!'
     }),
-    role: Joi.string().valid("ADMIN", "SUPERADMIN").messages({
-        'string.base': 'Role faqat matn boʻlishi kerak',
-        'any.only': 'Role faqat ADMIN yoki SUPERADMIN bo‘lishi mumkin',
+    role: Joi.string().empty('').valid("ADMIN", "SUPERADMIN").messages({
+        'string.base': 'Role faqat matn boʻlishi kerak!',
+        'string.empty': 'Role kiritilmagan!',
+        'any.only': 'Role faqat ADMIN yoki SUPERADMIN bo‘lishi mumkin!',
         'any.required': 'Role majburiy maydon!'
     })
 })
