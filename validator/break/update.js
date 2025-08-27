3b159d6538c595759bc0595110b05bcd7fd7dfb1
Avazbekcoder512 <qalandarovavazbek43@gmail.com>
2025-08-27 15:26:39 +0500

    test

--- validator/break/update.js
+++ validator/break/update.js
@@ -17,5 +17,11 @@ export const updateBreakSchema = Joi.object({
         .messages({
             'string.pattern.base': 'Vaqt HH:MM formatida bo‘lishi kerak',
             'string.empty': 'Vaqt bo‘sh bo‘lishi mumkin emas'
-        })
+        }),
+    lateAllow: Joi.number().empty().min(10).max(30).messages({
+        'number.base': 'Kech qolish vaqti raqamda kiritilsin!',
+        'number.empty': 'Kech qolish vaqti boʻsh boʻlmasligi kerak!',
+        'number.min': 'Kech qolish vaqti {#limit} dan kam boʻlmasligi kerak!',
+        'number.max': 'Kech qolish vaqti {#limit} dan oshmasligi kerak!',
+    })
 })
